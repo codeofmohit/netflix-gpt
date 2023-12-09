24d19b6dca1f13fa651fb54aa4a90f05a3259f69
codeofmohit <mohit156@gmail.com>
2023-12-09 13:08:53 +0530

    initial setup done

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-bold text-green-700 font-bold text-3xl">
+        Netflix GPT
+      </h1>
+    </div>
+  );
+}
+
+export default App;
