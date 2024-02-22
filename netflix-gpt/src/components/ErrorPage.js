b329ddac4cdb3098e73a034714c18fa2d3a295dd
codeofmohit <mohit156@gmail.com>
2024-02-22 07:07:44 +0530

    login component added

---
+++ netflix-gpt/src/components/ErrorPage.js
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      Oops! This link, either does not exist or you are not authorised to come
+      here!
+    </div>
+  );
+};
+export default ErrorPage;
---
+++ netflix-gpt/src/components/Header.js
@@ -0,0 +1,20 @@
+/* 
+
+Header : a comann header for both login and browse routes
+        - simple and neat
+        - will have logo that't it
+
+*/
+
+const Header = () => {
+  return (
+    <>
+      <img
+        className="absolute p-6 ml-16 bg-gradient-to-b from-transparent to-black w-64"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="logo"
+      />
+    </>
+  );
+};
+export default Header;
