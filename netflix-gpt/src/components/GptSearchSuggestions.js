59608316221d32103eac08ece1ba58852eb73f6b
codeofmohit <mohit156@gmail.com>
2024-03-18 20:56:08 +0530

    upated note for better UX

--- netflix-gpt/src/components/GptSearchSuggestions.js
+++ netflix-gpt/src/components/GptSearchSuggestions.js
@@ -9,8 +9,12 @@ const GptSearchSuggestions = () => {
 
   if (!movieSuggestions) {
     return (
-      <div className="absolute top-[22vh] md:top-[40vh] bg-[rgba(0,0,0,0.6)] m-0 md:m-8 mt-8 md:mt-[unset] rounded text-white ">
-        <h1 className="p-8">
+      <div className="p-8 absolute top-[22vh] md:top-[40vh] bg-[rgba(0,0,0,0.6)] m-0 md:m-8 mt-8 md:mt-[unset] rounded text-white ">
+        <h1 className="m-2 p-2 rounded font-bold text-lg text-slate-300 bg-[rgba(0,0,0,0.5)] inline-block">
+          Note : Do try searching first! <br></br>in case search does not work,
+          read below ⤵️
+        </h1>
+        <h1 className="m-2">
           Your're likely on a JIO network, in this feature we make use of TMDB
           api(s) which are not stable on jio, try switching your network or
           connect via a VPN such as VeePN Chrome extension, Thanks!
